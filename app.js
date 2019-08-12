bf78733783f811447e5e5e98b0853e4d4196aa33
DIPAK GHUGE <deepghuge@gmail.com>
2019-08-12 23:40:15 +0530

    add the original port format

--- app.js
+++ app.js
@@ -137,11 +137,6 @@ app.get("/about", function (req, res) {
   res.render("about");
 });
 
-let port = process.env.PORT;
-if (port == null || port == "") {
-  port = 3000;
-}
-app.listen(port);
-app.listen(port, function () {
-  console.log("Server started on port 3000");
+app.listen(process.env.PORT || 3000, function () {
+  console.log("sever is running on 3000");
 });
